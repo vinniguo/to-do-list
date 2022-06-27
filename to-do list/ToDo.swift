4d9a8deaddee4594d741a57c2af2e2910e38db16
Vinni Guo <guovinni@gmail.com>
2022-06-27 16:47:32 -0500

    iteration 1

---
+++ to-do list/ToDo.swift
@@ -0,0 +1,14 @@
+//
+//  ToDo.swift
+//  to-do list
+//
+//  Created by Scholar on 6/27/22.
+//
+
+import UIKit
+
+class ToDo{
+    var name = "u suck bro"
+    var important = false
+    
+}
--- to-do list/ToDoListTableViewController.swift
+++ to-do list/ToDoListTableViewController.swift
@@ -8,82 +8,57 @@
 import UIKit
 
 class ToDoListTableViewController: UITableViewController {
+  
+    var toDos : [ToDo] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        toDos = createToDos()
+        
+    } // END OF VIEWDIDLOAD FUNCTION
+    
+    func createToDos() -> [ToDo]{
+        let swift = ToDo()
+        swift.name = "show everyibe we know Swift"
+        swift.important = true
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
+        let cat = ToDo()
+        cat.name = "Let the Cat out"
+        cat.important = true
+        // important is set to false by default
 
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        return [swift, cat]
     }
+    
+    
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return toDos.count
     }
-
-    /*
+    
+    
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+      let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
 
-        // Configure the cell...
+      let toDo = toDos[indexPath.row]
+        
+        if toDo.important{
+            cell.textLabel?.text = "🤘" + toDo.name
+        } else{
+            cell.textLabel?.text = toDo.name
+        }
 
-        return cell
+        
+      return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
